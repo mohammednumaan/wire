3b65a04c8e4904e71ecf6778955f5df82e30aa0c
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2025-11-15 22:01:26 +0530

    feat: add initial monorepo structure using npm workspaces.

---
+++ packages/tui/components/app.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Text, render } from "ink";
+
+export default function App() {
+	return (
+		<Box>
+
+			<Text>
+				welcome to wire.
+			</Text>
+		</Box>
+	)
+}
+
+render(<App />);
